c1d2669547818c1ba1ad0d8bc44f81b877ea304b
lexilouds <lexiloudiadis@gmail.com>
2026-04-02 16:04:57 -0700

    is empty checks for table

--- database/db_connector.py
+++ database/db_connector.py
@@ -16,6 +16,10 @@ def connect_db():
     return cnx
 
 
+def drop_tables(table1, table2, table3, table4, cursor):
+    sql = f"DROP TABLE IF EXISTS {table1}, {table2}, {table3}, {table4}"
+    cursor.execute(sql)
+
 def create_tables(cursor):
     create_table1_statement = 'CREATE TABLE IF NOT EXISTS interprovincial_tourist_spending ' \
     '(year SMALLINT, province_of_residence VARCHAR(60), destination_province VARCHAR(60),' \
@@ -52,28 +56,30 @@ def insert_into_table1(cursor, df1):
     table1_insert = "INSERT INTO interprovincial_tourist_spending VALUES (%s, %s, %s, %s)"
     rows = [tuple(row) for row in df1.itertuples(index=False)]
     cursor.executemany(table1_insert, rows)
-    print(f"Inserted {cursor.rowcount} rows")
+    print(f"Inserted {cursor.rowcount} rows into interprovincial_tourist_spending")
 
 def insert_into_table2(cursor, df2):
     table2_insert = "INSERT INTO international_tourist_spending VALUES (%s, %s, %s, %s, %s)"
     rows = [tuple(row) for row in df2.itertuples(index=False)]
     cursor.executemany(table2_insert, rows)
-    print(f"Inserted {cursor.rowcount} rows")
+    print(f"Inserted {cursor.rowcount} rows into international_tourist_spending")
 
 def insert_into_table3(cursor, df3):
     table3_insert = "INSERT INTO tourism_supply_demand VALUES (%s, %s, %s, %s, %s)"
     rows = [tuple(row) for row in df3.itertuples(index=False)]
     cursor.executemany(table3_insert, rows)
-    print(f"Inserted {cursor.rowcount} rows")
+    print(f"Inserted {cursor.rowcount} rows into tourism_supply_demand")
 
 def insert_into_table4(cursor, df4):
     table4_insert = "INSERT INTO provincial_visitor_count VALUES (%s, %s, %s, %s)"
     split_size = 1000
     rows = [tuple(row) for row in df4.itertuples(index=False)]
 
+    total_rows = 0
     for i in range(0, len(rows), split_size):
         split = rows[i : i + split_size]
         cursor.executemany(table4_insert, split)
+        total_rows += cursor.rowcount
     
-    print(f"Inserted {cursor.rowcount} rows")
+    print(f"Inserted a total of {total_rows} / {len(rows)} rows into provincial_visitor_count")
 
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from data_load_and_clean.load_and_clean import load_and_clean_dataset1, load_and_clean_dataset2, load_and_clean_dataset3, load_and_clean_dataset4
-from database.db_connector import connect_db, create_tables, insert_into_table1, insert_into_table2, insert_into_table3, insert_into_table4
+from database.db_connector import connect_db, create_tables, insert_into_table1, insert_into_table2, insert_into_table3, insert_into_table4, drop_tables
+
+def is_empty(table, cursor):
+    sql = f"SELECT * FROM {table} LIMIT 10"
+    cursor.execute(sql)
+
+    if len(cursor.fetchall()) > 1:
+        return False
+    else:
+        return True
+
 
 def main():
     interprovincial_tourist_spending = load_and_clean_dataset1()
@@ -9,13 +19,31 @@ def main():
 
     cnx = connect_db()
     cursor = cnx.cursor()
+
+    # drop_tables('interprovincial_tourist_spending', 'international_tourist_spending', 'tourism_supply_demand', 'provincial_visitor_count', cursor)
     create_tables(cursor)
 
-    insert_into_table1(cursor, interprovincial_tourist_spending)
-    insert_into_table2(cursor, international_tourist_spending)
-    insert_into_table3(cursor, tourism_supply_demand)
-    insert_into_table4(cursor, provincial_visitor_count)
+    if is_empty('interprovincial_tourist_spending', cursor):
+        insert_into_table1(cursor, interprovincial_tourist_spending)
+    
+    if is_empty('international_tourist_spending', cursor):
+        insert_into_table2(cursor, international_tourist_spending)
+    
+    if is_empty('tourism_supply_demand', cursor):
+        insert_into_table3(cursor, tourism_supply_demand)
+    
+    if is_empty('provincial_visitor_count', cursor):
+        insert_into_table4(cursor, provincial_visitor_count)
 
+    cnx.commit()
+
+    cursor.execute("SELECT * FROM provincial_visitor_count LIMIT 10")
+    for row in cursor.fetchall():
+        print(row)
+
+    
+    cursor.close()
+    cnx.close()
 
 if __name__ == "__main__":
     main()
